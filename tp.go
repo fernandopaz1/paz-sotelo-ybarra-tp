6d3082c31d199d05acf1ea5ae3ac41c1ab6654bf
Nacho01 <nachosotelo9@gmail.com>
2020-11-26 10:43:49 -0300

    Agregamos func para tarjeta, comercio y compra, tambien agregamos algunos valores para sus tablas

--- tp.go
+++ tp.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     _ "github.com/lib/pq"
     "log"
+    "time"
 )
 
 func createDatabase() {
@@ -31,13 +32,71 @@ func createDatabase() {
 type cliente struct {
     nroCliente int
     nombre, apellido, domicilio string
-    //telefono [12] rune
+    telefono [12] rune
 }
 
+type comercio struct {
+    nroComercio int
+    nombre, domicilio, codPostal string
+    telefono [12] rune
+}
 
+type tarjeta struct {
+    nroCliente int
+    nroTarjeta [16] rune
+    validaDesde, validaHasta [6] rune
+    codigoSeguridad [4] rune
+    estado [10] rune
+    limiteCompra float64
+}
+
+type compra struct {
+    nroOperacion,nroComercio int
+    nroTarjeta [16] rune
+    fecha time.Time
+    monto float64
+    pagado bool
+}
+
+func insertarCompra(db *sql.DB, nroOperacion int ,nroTarjeta string, nroComercio int, fecha string, monto float64, pagado bool){
+	comandoSQL := fmt.Sprintf(`insert into compra values ('%v','%v','%v','%v','%v','%v');`,nroOperacion,stringtoSQLArray(nroTarjeta),nroComercio,fecha,monto,pagado)
+   fmt.Println(comandoSQL)
+   fmt.Println(fecha)
+   var err error
+    _, err = db.Exec(comandoSQL)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func insertarTarjeta(db *sql.DB, numeroTarjeta string ,nroCliente int, valDesde string, valHasta string, codigoSeguridad string, limiteCompra float64, estadO string){
+	nroTarjeta := stringtoSQLArray(numeroTarjeta)
+	validaDesde := stringtoSQLArray(valDesde)
+	validaHasta := stringtoSQLArray(valHasta)
+	codSeguridad := stringtoSQLArray(codigoSeguridad)
+	estado := stringtoSQLArray(estadO)
+	
+	comandoSQL := fmt.Sprintf(`insert into tarjeta values ('%v','%v','%v','%v','%v','%v','%v');`,
+	nroTarjeta,nroCliente,validaDesde,validaHasta,codSeguridad,limiteCompra,estado)
+   
+   var err error
+    _, err = db.Exec(comandoSQL)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func insertarComercio(db *sql.DB, nroComercio int ,nombre string, domicilio string, codPostal string, tel string){
+	comandoSQL := fmt.Sprintf(`insert into comercio values ('%v','%v','%v','%v','%v');`,nroComercio , nombre,domicilio,codPostal,stringtoSQLArray(tel))
+   var err error
+    _, err = db.Exec(comandoSQL)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
 func insertarClientes(db *sql.DB, nroCliente int ,nombre string, apellido string, domicilio string,tel string){
 	comandoSQL := fmt.Sprintf(`insert into cliente values ('%v','%v','%v','%v','%v');`,nroCliente,nombre,apellido,domicilio,stringtoSQLArray(tel))
-    var err error
+   var err error
     _, err = db.Exec(comandoSQL)
     if err != nil {
         log.Fatal(err)
@@ -58,12 +117,51 @@ func main() {
         fmt.Println("Error al crear cliente")
         log.Fatal(err)
     }
-	
+    
+    _, err = db.Exec(`create table comercio (nroComercio int ,nombre text, domicilio text, codPostal text, telefono char[])`)
+    if err != nil {
+        fmt.Println("Error al crear comercio")
+        log.Fatal(err)
+    }
+    
+    _, err = db.Exec(`create table tarjeta (numeroTarjeta char[] ,nroCliente int, valDesde char[], valHasta char[], codigoSeguridad char[], limiteCompra float, estado char[])`)
+    if err != nil {
+        fmt.Println("Error al crear tarjeta")
+        log.Fatal(err)
+    }
+    _, err = db.Exec(`create table compra (nroOperacion int ,nroTarjeta char[], nroComercio int, fecha date, monto float, pagado boolean)`)
+    if err != nil {
+        fmt.Println("Error al crear compra")
+        log.Fatal(err)
+    }
+
     tel :="111111111111"
+    telComercio := "222222222222"
+    layout := "2006-01-02"
+    updatedAt, _ := time.Parse(layout, "2016-06-10") // lee un string y lo transforma a formato fecha
+    fechita :=updatedAt.Format("2006-01-02") // acorta la fecha sin min y zona horaria 
+    
+    //var limCompra float64 
+    //limCompra = 30000.50
+    
     insertarClientes(db,1,"Fernando","Paz","Calle falsa",tel)
 
     insertarClientes(db,2,"Nacho","Sotelo","Callemasfalsa",tel)
-
+    
+    insertarClientes(db,3,"Flavio","Ybarra","Calle 123",tel)
+    
+    insertarClientes(db,4,"Florentina","Sosa","Fonrouge 3870",tel)
+    
+    insertarClientes(db,5,"Verónica","Roldán","Callemasfalsa",tel)
+    
+    
+    insertarComercio(db,1,"Rey del pancho","Pte Peron 222","1613",telComercio)
+    
+    insertarTarjeta(db,"1234567891011123",1,"202005","202205","1234",30000.50,"suspendida")
+    
+    insertarCompra(db,1,"1234567891011123",3156,fechita,122.2,true)
+    
+    
 }
 
 func stringtoSQLArray(s string) string{
