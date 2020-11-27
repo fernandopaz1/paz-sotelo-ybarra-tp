2a317454003bde167dec9e71cc0aaf797e180d36
Flavio Ybarra <flavio_712@hotmail.com>
2020-11-27 09:49:33 -0300

    creamos la funcion cargar datos

--- tp.go
+++ tp.go
@@ -58,52 +58,7 @@ type compra struct {
     pagado bool
 }
 
-func insertarCompra(db *sql.DB, nroOperacion int ,nroTarjeta string, nroComercio int, fecha string, monto float64, pagado bool){
-	comandoSQL := fmt.Sprintf(`insert into compra values ('%v','%v','%v','%v','%v','%v');`,nroOperacion,stringtoSQLArray(nroTarjeta),nroComercio,fecha,monto,pagado)
-   fmt.Println(comandoSQL)
-   fmt.Println(fecha)
-   var err error
-    _, err = db.Exec(comandoSQL)
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
-func insertarTarjeta(db *sql.DB, numeroTarjeta string ,nroCliente int, valDesde string, valHasta string, codigoSeguridad string, limiteCompra float64, estadO string){
-	nroTarjeta := stringtoSQLArray(numeroTarjeta)
-	validaDesde := stringtoSQLArray(valDesde)
-	validaHasta := stringtoSQLArray(valHasta)
-	codSeguridad := stringtoSQLArray(codigoSeguridad)
-	estado := stringtoSQLArray(estadO)
-	
-	comandoSQL := fmt.Sprintf(`insert into tarjeta values ('%v','%v','%v','%v','%v','%v','%v');`,
-	nroTarjeta,nroCliente,validaDesde,validaHasta,codSeguridad,limiteCompra,estado)
-   
-   var err error
-    _, err = db.Exec(comandoSQL)
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
-func insertarComercio(db *sql.DB, nroComercio int ,nombre string, domicilio string, codPostal string, tel string){
-	comandoSQL := fmt.Sprintf(`insert into comercio values ('%v','%v','%v','%v','%v');`,nroComercio , nombre,domicilio,codPostal,stringtoSQLArray(tel))
-   var err error
-    _, err = db.Exec(comandoSQL)
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-func insertarClientes(db *sql.DB, nroCliente int ,nombre string, apellido string, domicilio string,tel string){
-	comandoSQL := fmt.Sprintf(`insert into cliente values ('%v','%v','%v','%v','%v');`,nroCliente,nombre,apellido,domicilio,stringtoSQLArray(tel))
-   var err error
-    _, err = db.Exec(comandoSQL)
-    if err != nil {
-        log.Fatal(err)
-    }
-}
-
-func main() {
+func cargarDatos() {
     createDatabase()
     db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
     if err != nil {
@@ -124,44 +79,36 @@ func main() {
 
     _, err = db.Exec(tabla)
     if err != nil {
-        fmt.Println("Error al crear tabla")
+        fmt.Println("Error al crear las tablas")
         log.Fatal(err)
     }
-
-    tel :="111111111111"
-    //telComercio := "222222222222"
-    //layout := "2006-01-02"
-    //updatedAt, _ := time.Parse(layout, "2016-06-10") // lee un string y lo transforma a formato fecha
-    //fechita :=updatedAt.Format("2006-01-02") // acorta la fecha sin min y zona horaria 
     
-    //var limCompra float64 
-    //limCompra = 30000.50
+    datosClientes := `insert into cliente values ('1','Fernando','Paz','callao 345','{"1","1","3","4","5","6","8","7","6","5"}')`
     
-    insertarClientes(db,1,"Fernando","Paz","Calle falsa",tel)
-
-    insertarClientes(db,2,"Nacho","Sotelo","Callemasfalsa",tel)
-    
-    insertarClientes(db,3,"Flavio","Ybarra","Calle 123",tel)
-    
-    insertarClientes(db,4,"Florentina","Sosa","Fonrouge 3870",tel)
-    
-    insertarClientes(db,5,"Verónica","Roldán","Callemasfalsa",tel)
-    
-    
-    //insertarComercio(db,1,"Rey del pancho","Pte Peron 222","1613",telComercio)
-    
-    //insertarTarjeta(db,"1234567891011123",1,"202005","202205","1234",30000.50,"suspendida")
-    
-    //insertarCompra(db,1,"1234567891011123",3156,fechita,122.2,true)
-    
-    
-}
-
-func stringtoSQLArray(s string) string{
-    nuevo :=`{`
-    for i:=0; i<len(s)-1; i++ {
-        nuevo += `"`+string(s[i])+`",`
+	datosComercios := `insert into comercio values ('1','Rey del pancho','Pte peron 3221','1613','{"2","2","3","4","5","6","8","7","6","5"}')`
+	
+	datosTarjetas := `insert into tarjeta values ('{"1","1","3","4","5","6","8","7","6","5","5","6","8","7","6","5"}','2','{"5","6","8","7","6","5"}','{"5","6","8","7","6","4"}','{"9","6","8","7"}','121212.12','{"v","i","g","e","n","t","e"}')`
+	
+	_, err = db.Exec(datosClientes)
+    if err != nil {
+        fmt.Println("Error al cargar clientes")
+        log.Fatal(err)
     }
-    nuevo+= `"`+string(s[len(s)-1])+`"}`
-    return nuevo
+    
+    _, err = db.Exec(datosComercios)
+    if err != nil {
+        fmt.Println("Error al cargar los comercios")
+        log.Fatal(err)
+    }
+    
+   _, err = db.Exec(datosTarjetas)
+    if err != nil {
+        fmt.Println("Error al cargar las tarjetas")
+        log.Fatal(err)
+    }
 }
+	
+func main (){
+	cargarDatos()
+}  
+
