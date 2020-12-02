c84844f6edacb6e58654b038e554302d6a2e7fff
Flavio Ybarra <flavio_712@hotmail.com>
2020-12-01 21:46:32 -0300

    Se subio cambios en el trigger rechazo

--- codigo/crearTablas.sql
+++ codigo/crearTablas.sql
@@ -2,9 +2,9 @@ create table cliente (nroCliente int ,nombre text, apellido  text, domicilio tex
 create table comercio (nroComercio int ,nombre text, domicilio text, codPostal text, telefono char[]);
 create table tarjeta (nroTarjeta char[] ,nroCliente int, valDesde char[], valHasta char[], codigoSeguridad char[], limiteCompra float, estado char[]);
 create table compra (nroOperacion int ,nroTarjeta char[], nroComercio int, fecha timestamp, monto float, pagado boolean);
-create table rechazo (nroRechazo serial, nroTarjeta char[], nroComercio int, fecha date, monto float, motivo text);
+create table rechazo (nroRechazo serial, nroTarjeta char[], nroComercio int, fecha timestamp, monto float, motivo text);
 create table cierre (año int, mes int, terminacion int, fechaInicio date, fechaCierre date, fechaVto date);
 create table cabecera (nroResumen serial, nombre text, apellido text, domicilio text, nroTarjeta char[], desde date, hasta date, vence date, total float);
 create table detalle (nroResumen int, nroLinea serial, fecha date, nombreComercio text, monto float);
-create table alerta (nroAlerta serial, nroTarjeta char[], fecha date, nroRechazo int, codAlerta int, descripcion text);
+create table alerta (nroAlerta serial, nroTarjeta char[], fecha timestamp, nroRechazo int, codAlerta int, descripcion text);
 create table consumo (nroTarjeta char[], codigoSeguridad char[], nroComercio int, monto float)
--- codigo/triggerRechazo.sql
+++ codigo/triggerRechazo.sql
@@ -1,6 +1,13 @@
 create or replace function crear_alerta()  returns trigger as $$
+	declare
+		increment_value  interval := interval '1 day';
+		cantRechazo int;
 	begin
 		insert into alerta values(default, new.nroTarjeta, new.fecha , new.nroRechazo, 0, new.motivo);
+		select count(*) into cantRechazo from rechazo where nroTarjeta = new.nroTarjeta and fecha - new.fecha < increment_value and new.motivo = 'supera límite de tarjeta';
+		if (cantRechazo > 1) then
+			insert into alerta values(default, new.nroTarjeta, new.fecha , new.nroRechazo, 32,'Tarjeta suspendida por varios excesos de limite');
+		end if;
 		return new;
 	end; 
 $$ language plpgsql;
