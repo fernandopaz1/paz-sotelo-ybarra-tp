9e84d1187879d20f3645359e7954fdc19e07369b
Nacho01 <nachosotelo9@gmail.com>
2020-12-02 12:36:22 -0300

    terminamos el trigger para las alertas

--- codigo/triggerCompra.sql
+++ codigo/triggerCompra.sql
@@ -5,25 +5,39 @@ create or replace function crear_alerta_compras()  returns trigger as $$
 		cantComprasUnMin int;
 		cantComprasCincoMin int;
 		cp text;
-
+		
 	begin
-		select codPostal into cp from comercio where nroComercio in (select nroComercio from compra where nroTarjeta = new.nroTarjeta);
 		
-		select count(*) into cantComprasUnMin from compra c,comercio com where c.nroTarjeta = new.nroTarjeta 
-		and c.fecha - new.fecha < tiempoCompra and cp = com.codPostal and c.nroComercio != new.nroComercio;
-		
-		select count(*) into cantComprasCincoMin from compra c,comercio com where
-		 c.nroTarjeta = new.nroTarjeta and c.fecha - new.fecha < tiempoCompraCinco and cp != com.codPostal;
+		 select codPostal into cp from comercio where nroComercio = new.nroComercio;
 		
 		
-		if (cantComprasUnMin > 2) then
-			insert into alerta values(default, new.nroTarjeta, new.fecha , null , 1,'Se detectaron mas de 1 compra en 1 minuto');
-		return new;	
-		elsif cantComprasCincoMin > 2 then
+		select count (codPostal) into cantComprasUnMin from comercio where nroComercio in 
+		(select distinct nroComercio from compra c where c.nroTarjeta = new.nroTarjeta and c.fecha - new.fecha < tiempoCompra );
+
+		
+		select count (distinct codPostal) into cantComprasCincoMin from comercio where nroComercio in 
+		(select distinct nroComercio from compra c where c.nroTarjeta = new.nroTarjeta and c.fecha - new.fecha < tiempoCompraCinco);
+
+		
+		
+		if (cantComprasCincoMin > 1) then
 			insert into alerta values(default, new.nroTarjeta, new.fecha , null , 5,'Se detectaron mas de 1 compra en 5 minutos');
-		return new;	
-		end if;
+			
+			return new;
+		end if;  	
+		
+		if cantComprasUnMin > 1 then
+			
+			insert into alerta values(default, new.nroTarjeta, new.fecha , null , 1,'Se detectaron mas de 1 compra en 1 minuto');
+			return new;
+		
+		end if ;	
+		
+		
 		return new;
+		
+		
+	
 	end; 
 $$ language plpgsql;
 create trigger alerta_automatica_compras_trg
