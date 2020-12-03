83f2d8fa9023170eec2a6b604870dff538c43d2a
fernandopaz1 <ferpaz9292@gmail.com>
2020-12-02 22:10:24 -0300

    agregamos lass funciones al menu

--- tp.go
+++ tp.go
@@ -3,15 +3,15 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	bolt "github.com/coreos/bbolt"
 	_ "github.com/lib/pq"
 	"io/ioutil"
 	"log"
 	"os"
-	"time"
 	"strconv"
-	"encoding/json"
+	"time"
 )
 
 func createDatabase() {
@@ -71,7 +71,6 @@ type Consumo struct {
 }
 
 func cargarDatos() {
-	createDatabase()
 	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
 	if err != nil {
 		fmt.Println("Error al abrir la base de datos ya creada")
@@ -89,6 +88,7 @@ func cargarDatos() {
 
 	cargarCierre(db, 2020)
 
+	cargarComandosAPostgres(db, "codigo/datosConsumos.sql")
 }
 
 //conviene usar una funcion en Go por el manejo de ciclos
@@ -117,6 +117,152 @@ func cargarCierre(db *sql.DB, anio int) {
 	}
 }
 
+func main() {
+	menu()
+}
+
+func menu() {
+	fmt.Print("\033[H\033[2J") //Limpia la terminal
+
+	fmt.Println(`Intsroduzca la opcion elegida :
+				1. Para crear la base de datos
+				2. Para cargar datos
+				3. Para agregar las Pk y Fk
+				4. Para cargar los stored procedures y triggers
+				5. Testear base usando consumo
+				6. Borrar Pks  y Fks
+				7. Cargar base Bolt.db
+				q. Salir`)
+	reader := bufio.NewReader(os.Stdin)
+	char, _, err := reader.ReadRune()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	switch char {
+	case '1':
+		createDatabase()
+		fmt.Println("Creando")
+
+		break
+	case '2':
+		cargarDatos()
+		fmt.Println("cargando la base")
+		break
+	case '3':
+		cargarPkYFK()
+		fmt.Println("verificando stored procedures")
+		break
+	case '4':
+		cargarProceduresYTriggers()
+		fmt.Println("verificando stored procedures")
+		break
+	case '5':
+		testearBaseConConsumo()
+		fmt.Println("Cargando en NoSQL")
+		break
+	case '6':
+		borrarKeys()
+		fmt.Println("Cargando en NoSQL")
+		break
+	case '7':
+		crearBoltDB()
+		fmt.Println("Cargando en NoSQL")
+		break
+
+	case 'q':
+		fmt.Println("Chau")
+		return;
+		break
+	default:
+		fmt.Println("La opcion elegida no es valida")
+		time.Sleep(2 * time.Second)
+	}
+	menu()
+}
+
+func cargarPkYFK() {
+	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
+	if err != nil {
+		fmt.Println("Error al abrir la base de datos ya creada")
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	cargarComandosAPostgres(db, "codigo/pks.sql")
+	cargarComandosAPostgres(db, "codigo/fks.sql")
+
+}
+
+func cargarComandosAPostgres(db *sql.DB, path string) {
+	file, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request := string(file)
+
+	_, err = db.Exec(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cargarProceduresYTriggers() {
+	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
+	if err != nil {
+		fmt.Println("Error al abrir la base de datos ya creada")
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	cargarComandosAPostgres(db, "codigo/funcionesAuxiliares.sql")
+
+	cargarComandosAPostgres(db, "codigo/autorizacionDeCompra.sql")
+
+	cargarComandosAPostgres(db, "codigo/generacionDeResumen.sql")
+
+	cargarComandosAPostgres(db, "codigo/triggerRechazo.sql")
+
+	cargarComandosAPostgres(db, "codigo/triggerCompra.sql")
+
+
+}
+
+func testearBaseConConsumo() {
+	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
+	if err != nil {
+		fmt.Println("Error al abrir la base de datos ya creada")
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`select cargar_consumos_en_compra()`)
+	if err != nil {
+		fmt.Println("Error al cargar el consumo")
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`select generar_resumenes_del_anio()`)
+	if err != nil {
+		fmt.Println("Error al cargar el consumo")
+		log.Fatal(err)
+	}
+}
+
+func borrarKeys() {
+	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
+	if err != nil {
+		fmt.Println("Error al abrir la base de datos ya creada")
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	cargarComandosAPostgres(db, "codigo/removeKeys.sql")
+}
+
 func crearBoltDB() {
 
 	db, err := bolt.Open("bolt.db", 0600, nil)
@@ -124,10 +270,10 @@ func crearBoltDB() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	
+
 	// se cargan los cliente
-	
-	fernando := Cliente{1,"Fernando","Paz","Callao 345",[12]rune{'1','1','3','4','5','6','8','7','6','5','6','5'}}
+
+	fernando := Cliente{1, "Fernando", "Paz", "Callao 345", [12]rune{'1', '1', '3', '4', '5', '6', '8', '7', '6', '5', '6', '5'}}
 	data, err := json.Marshal(fernando)
 	if err != nil {
 		log.Fatal(err)
@@ -135,8 +281,8 @@ func crearBoltDB() {
 	CreateUpdate(db, "cliente", []byte(strconv.Itoa(fernando.NroCliente)), data)
 	resultado1, err := ReadUnique(db, "cliente", []byte(strconv.Itoa(fernando.NroCliente)))
 	fmt.Printf("%s\n", resultado1)
-	
-	manolo := Cliente{2,"Manolo","Lettiere","Matheu 3942",[12]rune{'1','1','4','7','5','4','4','3','6','0'}}
+
+	manolo := Cliente{2, "Manolo", "Lettiere", "Matheu 3942", [12]rune{'1', '1', '4', '7', '5', '4', '4', '3', '6', '0'}}
 	data, err = json.Marshal(manolo)
 	if err != nil {
 		log.Fatal(err)
@@ -144,8 +290,8 @@ func crearBoltDB() {
 	CreateUpdate(db, "cliente", []byte(strconv.Itoa(manolo.NroCliente)), data)
 	resultado2, err := ReadUnique(db, "cliente", []byte(strconv.Itoa(manolo.NroCliente)))
 	fmt.Printf("%s\n", resultado2)
-	
-	carlota := Cliente{3,"Carlota","Correa","San Martin 975",[12]rune{'1','1','9','4','4','2','7','7','3','5'}}
+
+	carlota := Cliente{3, "Carlota", "Correa", "San Martin 975", [12]rune{'1', '1', '9', '4', '4', '2', '7', '7', '3', '5'}}
 	data, err = json.Marshal(carlota)
 	if err != nil {
 		log.Fatal(err)
@@ -153,10 +299,10 @@ func crearBoltDB() {
 	CreateUpdate(db, "cliente", []byte(strconv.Itoa(carlota.NroCliente)), data)
 	resultado3, err := ReadUnique(db, "cliente", []byte(strconv.Itoa(carlota.NroCliente)))
 	fmt.Printf("%s\n", resultado3)
-	
+
 	// se cargan los comercio
-	
-	adidas := Comercio{1,"Adidas","Pte peron 3221","1643",[12]rune{'1','1','4','9','2','1','1','9','7','1'}}
+
+	adidas := Comercio{1, "Adidas", "Pte peron 3221", "1643", [12]rune{'1', '1', '4', '9', '2', '1', '1', '9', '7', '1'}}
 	data, err = json.Marshal(adidas)
 	if err != nil {
 		log.Fatal(err)
@@ -164,8 +310,8 @@ func crearBoltDB() {
 	CreateUpdate(db, "comercio", []byte(strconv.Itoa(adidas.NroComercio)), data)
 	resultado4, err := ReadUnique(db, "comercio", []byte(strconv.Itoa(adidas.NroComercio)))
 	fmt.Printf("%s\n", resultado4)
-	
-	nike := Comercio{2,"Nike","Miraflores 2121","1643",[12]rune{'1','1','4','4','5','1','8','7','6','5'}}
+
+	nike := Comercio{2, "Nike", "Miraflores 2121", "1643", [12]rune{'1', '1', '4', '4', '5', '1', '8', '7', '6', '5'}}
 	data, err = json.Marshal(nike)
 	if err != nil {
 		log.Fatal(err)
@@ -173,20 +319,19 @@ func crearBoltDB() {
 	CreateUpdate(db, "comercio", []byte(strconv.Itoa(nike.NroComercio)), data)
 	resultado5, err := ReadUnique(db, "comercio", []byte(strconv.Itoa(nike.NroComercio)))
 	fmt.Printf("%s\n", resultado5)
-	
-	mc_donals := Comercio{3,"Mc Donals","French 231","1643",[12]rune{'1','1','4','4','1','1','0','9','6','5'}}
-	data, err = json.Marshal(mc_donals)
+
+	mcDonals := Comercio{3, "Mc Donals", "French 231", "1643", [12]rune{'1', '1', '4', '4', '1', '1', '0', '9', '6', '5'}}
+	data, err = json.Marshal(mcDonals)
 	if err != nil {
 		log.Fatal(err)
 	}
-	CreateUpdate(db, "comercio", []byte(strconv.Itoa(mc_donals.NroComercio)), data)
-	resultado6, err := ReadUnique(db, "comercio", []byte(strconv.Itoa(mc_donals.NroComercio)))
+	CreateUpdate(db, "comercio", []byte(strconv.Itoa(mcDonals.NroComercio)), data)
+	resultado6, err := ReadUnique(db, "comercio", []byte(strconv.Itoa(mcDonals.NroComercio)))
 	fmt.Printf("%s\n", resultado6)
-	
+
 	// se cargan compras
-	
-	
-	compra1 := Compra{1,1,[16]rune{'5','1','5','4','5','6','8','7','6','5','5','6','8','7','6','5'},2020-11-27,150.50,false}
+
+	compra1 := Compra{1, 1, [16]rune{'5', '1', '5', '4', '5', '6', '8', '7', '6', '5', '5', '6', '8', '7', '6', '5'}, stringATime("2020-11-27"), 150.50, false}
 	data, err = json.Marshal(compra1)
 	if err != nil {
 		log.Fatal(err)
@@ -194,8 +339,8 @@ func crearBoltDB() {
 	CreateUpdate(db, "compra", []byte(strconv.Itoa(compra1.NroOperacion)), data)
 	resultado7, err := ReadUnique(db, "compra", []byte(strconv.Itoa(compra1.NroOperacion)))
 	fmt.Printf("%s\n", resultado7)
-	
-	compra2 := Compra{2,[16]rune{'4','0','3','4','1','6','1','7','6','5','2','2','8','0','6','5'},3,2020-11-27,150.50,false}
+
+	compra2 := Compra{2, 3, [16]rune{'4', '0', '3', '4', '1', '6', '1', '7', '6', '5', '2', '2', '8', '0', '6', '5'}, stringATime("2020-11-27"), 150.50, false}
 	data, err = json.Marshal(compra2)
 	if err != nil {
 		log.Fatal(err)
@@ -203,8 +348,8 @@ func crearBoltDB() {
 	CreateUpdate(db, "compra", []byte(strconv.Itoa(compra2.NroOperacion)), data)
 	resultado8, err := ReadUnique(db, "compra", []byte(strconv.Itoa(compra2.NroOperacion)))
 	fmt.Printf("%s\n", resultado8)
-	
-	compra3 := Compra{3,[16]rune{'5','5','3','4','5','6','4','7','3','3','5','6','8','5','5','1'},3,2020-11-27,150000.50,false}
+
+	compra3 := Compra{3, 3, [16]rune{'5', '5', '3', '4', '5', '6', '4', '7', '3', '3', '5', '6', '8', '5', '5', '1'}, stringATime("2020-11-27"), 150000.50, false}
 	data, err = json.Marshal(compra3)
 	if err != nil {
 		log.Fatal(err)
@@ -212,7 +357,35 @@ func crearBoltDB() {
 	CreateUpdate(db, "compra", []byte(strconv.Itoa(compra3.NroOperacion)), data)
 	resultado9, err := ReadUnique(db, "compra", []byte(strconv.Itoa(compra3.NroOperacion)))
 	fmt.Printf("%s\n", resultado9)
-	
+
+	// cargando tarjetas
+
+	tarjeta1 := Tarjeta{2, [16]rune{'5', '4', '2', '2', '5', '6', '8', '1', '6', '2', '5', '3', '8', '7', '6', '5'}, [6]rune{'2', '0', '1', '2', '0', '2'}, [6]rune{'2', '0', '2', '8', '0', '2'}, [4]rune{'2', '4', '9', '2'}, [10]rune{'v', 'i', 'g', 'e', 'n', 't', 'e'}, 70000.00}
+	data, err = json.Marshal(tarjeta1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	CreateUpdate(db, "tarjeta", []byte(strconv.Itoa(tarjeta1.NroCliente)), data)
+	resultado10, err := ReadUnique(db, "tarjeta", []byte(strconv.Itoa(tarjeta1.NroCliente)))
+	fmt.Printf("%s\n", resultado10)
+
+	tarjeta2 := Tarjeta{3, [16]rune{'5', '5', '3', '4', '5', '6', '4', '7', '3', '3', '5', '6', '8', '5', '5', '1'}, [6]rune{'2', '0', '1', '3', '0', '1'}, [6]rune{'2', '0', '2', '9', '0', '2'}, [4]rune{'4', '4', '8', '2'}, [10]rune{'v', 'i', 'g', 'e', 'n', 't', 'e'}, 70000.00}
+	data, err = json.Marshal(tarjeta2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	CreateUpdate(db, "tarjeta", []byte(strconv.Itoa(tarjeta2.NroCliente)), data)
+	resultado11, err := ReadUnique(db, "tarjeta", []byte(strconv.Itoa(tarjeta2.NroCliente)))
+	fmt.Printf("%s\n", resultado11)
+
+	tarjeta3 := Tarjeta{5, [16]rune{'5', '3', '3', '2', '5', '9', '8', '9', '6', '3', '3', '6', '1', '7', '6', '2'}, [6]rune{'2', '0', '1', '3', '0', '4'}, [6]rune{'2', '0', '2', '1', '0', '1'}, [4]rune{'2', '1', '6', '3'}, [10]rune{'v', 'i', 'g', 'e', 'n', 't', 'e'}, 60000.00}
+	data, err = json.Marshal(tarjeta3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	CreateUpdate(db, "tarjeta", []byte(strconv.Itoa(tarjeta3.NroCliente)), data)
+	resultado12, err := ReadUnique(db, "tarjeta", []byte(strconv.Itoa(tarjeta3.NroCliente)))
+	fmt.Printf("%s\n", resultado12)
 
 }
 
@@ -246,108 +419,11 @@ func ReadUnique(db *bolt.DB, bucketName string, key []byte) ([]byte, error) {
 	return buf, err
 }
 
-func main() {
-	//menu()
-
-	cargarDatos()
-
-	db, err := sql.Open("postgres", "user=postgres host=localhost dbname=transacciones sslmode=disable")
-	if err != nil {
-		fmt.Println("Error al abrir la base de datos ya creada")
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	cargarPkYFK(db)
-	cargarComandosAPostgres(db, "codigo/funcionesAuxiliares.sql")
-
-	cargarComandosAPostgres(db, "codigo/autorizacionDeCompra.sql")
-
-	cargarComandosAPostgres(db, "codigo/generacionDeResumen.sql")
-
-	cargarComandosAPostgres(db, "codigo/triggerRechazo.sql")
-
-	cargarComandosAPostgres(db, "codigo/triggerCompra.sql")
-
-	cargarComandosAPostgres(db, "codigo/datosConsumos.sql")
-
-	_, err = db.Exec(`select cargar_consumos_en_compra()`)
-	if err != nil {
-		fmt.Println("Error al cargar el consumo")
-		log.Fatal(err)
-	}
-
-	_, err = db.Exec(`select generar_resumenes_del_anio()`)
-	if err != nil {
-		fmt.Println("Error al cargar el consumo")
-		log.Fatal(err)
-	}
-
-	cargarComandosAPostgres(db, "codigo/removeKeys.sql")
-	
-	crearBoltDB()
-}
-
-func menu() {
-	fmt.Print("\033[H\033[2J") //Limpia la terminal
-
-	fmt.Println(`Intsroduzca la opcion elegida :
-                1. Para crear la base de datos
-                2. Para agregar las Pk y Fk 
-                3. Para cargar la tabla 
-                4. Para verificar los stored procedures
-                5. Carga los mismos datos en NoSQL
-                q. Salir`)
-	reader := bufio.NewReader(os.Stdin)
-	char, _, err := reader.ReadRune()
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	switch char {
-	case '1':
-		fmt.Println("Creando")
-		break
-	case '2':
-		fmt.Println("verificando stored procedures")
-		break
-	case '3':
-		fmt.Println("cargando la base")
-		break
-	case '4':
-		fmt.Println("verificando stored procedures")
-		break
-	case '5':
-		fmt.Println("Cargando en NoSQL")
-		break
-	case 'q':
-		fmt.Println("Chau")
-		break
-	default:
-		fmt.Println("La opcion elegida no es valida")
-		time.Sleep(2 * time.Second)
-		menu()
-	}
-}
-
-func cargarPkYFK(db *sql.DB) {
-	cargarComandosAPostgres(db, "codigo/pks.sql")
-	cargarComandosAPostgres(db, "codigo/fks.sql")
-
-}
-
-func cargarComandosAPostgres(db *sql.DB, path string) {
-	file, err := ioutil.ReadFile(path)
-
+func stringATime(str string) (t time.Time) {
+	layout := "2006-01-02"
+	t, err := time.Parse(layout, str)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	request := string(file)
-
-	_, err = db.Exec(request)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return t
 }
